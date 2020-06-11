a5b899cd84f04874a555aab75c63fa5714b43733
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-11 18:08:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-      new_string << src[row_index][element_index].join('')
+      new_string += src[row_index][element_index].join('')
     end
     element_index += 1
   end
